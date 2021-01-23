fbde151efd8de5345bc94bc87bf787e4f71cb828
madelinelise <madelinelise@gmail.com>
2021-01-23 16:43:26 -0500

    Updates App.js and removes unused files.

--- mj-fed-design-sys/src/App.js
+++ mj-fed-design-sys/src/App.js
@@ -1,32 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-import { Pager } from "./components/pager/pager"
-import { PersonCard } from "./components/person-card/person-card"
+import { useEffect, useState } from "react";
+import Users from "./components/users/users";
+import { Heading } from "./components/heading/heading";
+import Pagination from "./components/pagination/pagination";
+import axios from "axios";
+import { USER_PER_PAGE } from "./global/constants";
 
-function App(args) {
+function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      setLoading(true);
+      const res = await axios.get(
+        "https://randomuser.me/api/?page=1&results=50&nat=us"
+      );
+      setLoading(false);
+      setUsers(res.data.results);
+
+      setTotalPages(Math.ceil(res.data.results.length / USER_PER_PAGE));
+    };
+    fetchUsers();
+  }, []);
+
+  const handleClick = (num) => {
+    setPage(num);
+  };
+
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-
-
-     <h1>I'll add stuff here</h1>
-
-     <Pager/>
-     <PersonCard {...args} />
-
+    <div className="layout">
+      <Heading title="Our awesome team" headingLevel="h1" />
+      <Heading
+        title="This is an awesome subtitle."
+        headingLevel="h2"
+      />
+      <p>Page {page}</p>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <Users users={users} page={page} />
+          <Pagination totalPages={totalPages} handleClick={handleClick} />
+        </>
+      )}
     </div>
   );
 }
