e26524e932689ea012eb1d36dc29f73888e143db
madelinelise <madelinelise@gmail.com>
2021-01-24 16:35:46 -0500

    Adds download link to users.js.

--- mj-fed-design-sys/src/components/users/users.js
+++ mj-fed-design-sys/src/components/users/users.js
@@ -1,13 +1,31 @@
 import React from "react";
-import UserCard from "../user-card/user-card";
+import { CSVLink, CSVDownload } from "react-csv";
 import { USER_PER_PAGE } from "../../global/constants";
+import UserCard from "../user-card/user-card";
+import "./users.scss";
+import "../button/button.scss";
 
-const users = ({ users, page }) => {
+const Users = ({ users, page }) => {
   const startIndex = (page - 1) * USER_PER_PAGE;
   const selectedUsers = users.slice(startIndex, startIndex + USER_PER_PAGE);
-  return selectedUsers.map((user) => (
-    <UserCard user={user} key={user.login.uuid} />
-  ));
+
+  const userList = selectedUsers.map((user) => <UserCard user={user} />);
+
+  return (
+    <>
+      <CSVLink
+        className="button"
+        tabindex="0"
+        role="button"
+        id="toggle"
+        aria-pressed="false"
+        data={selectedUsers}
+      >
+        Download csv
+      </CSVLink>
+      <ul className="user-cards">{userList}</ul>
+    </>
+  );
 };
 
-export default users;
+export default Users;
