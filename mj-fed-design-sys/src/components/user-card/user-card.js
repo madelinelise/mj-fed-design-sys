81b9c5987fdfa1e0459341ffa003800010236d66
madelinelise <madelinelise@gmail.com>
2021-01-24 16:37:44 -0500

    Updates user card to have links for email and phone.

--- mj-fed-design-sys/src/components/user-card/user-card.js
+++ mj-fed-design-sys/src/components/user-card/user-card.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./user-card.scss";
 
 const UserCard = ({ user }) => {
-  console.log(user);
   return (
     <div className="user-card">
       <div className="user-card__image">
@@ -12,11 +11,15 @@ const UserCard = ({ user }) => {
         <h3 className="user-card__name user-card__info-item">
           {user.name.first} {user.name.last}
         </h3>
-        <span className="user-card__info-item">{user.email}</span>
-        <span className="user-card__info-item">{user.cell}</span>
         <span className="user-card__info-item">
           {user.dob.age} year old {user.gender}
         </span>
+        <a href={`${user.email}`} className="user-card__info-item">
+          {user.email}
+        </a>
+        <a href={`${user.cell}`} className="user-card__info-item">
+          {user.cell}
+        </a>
       </div>
     </div>
   );
